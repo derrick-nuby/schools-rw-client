bd92f511ec27eae6d071987e9f6d5e2ab007c4bc
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-08-30 19:15:51 +0200

    main comp setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-function App() {
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import MainUserLayout from './layout/MainUserLayout';
+import NotFoundPage from './pages/NotFoundPage';
+import HomePage from './pages/HomePage';
 
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<MainUserLayout />} >
+          <Route index element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          {/* 
+          <Route
+            path="/jobs/apply/expo2024"
+            element={<ApplyInternshipPage />}
+          /> */}
+        </Route >
+      </>
+    )
+  );
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <RouterProvider router={router} />
   );
 }
 
